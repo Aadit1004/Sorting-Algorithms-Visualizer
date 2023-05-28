a4a296234ff36b9f3d11a67194078c02d96cc779
Aadit Rao <aaditrao10@gmail.com>
2023-05-28 11:35:47 +0400

    Changed so that bubble sort is in the top left corner with less # of elems

--- SortingAlgorithmsVisualizer/main.cpp
+++ SortingAlgorithmsVisualizer/main.cpp
@@ -3,41 +3,48 @@
 #include <algorithm>
 #include <functional>
 
-int main() {
+const int winWidth = 800, winHeight = 450, rectWidth = 4;
 
-	const int winWidth = 800, winHeight = 450, rectWidth = 5;
+void drawCross() {
+	DrawRectangle(0, (winHeight / 2) - 5, winWidth, 5, BLACK);
+	DrawRectangle((winWidth / 2) - 5, 0, 5, winHeight, BLACK);
+}
+
+int main() {
 
 	InitWindow(winWidth, winHeight, "Sorting Algorithms Visualizer");
 
-	SetTargetFPS(60);
+	int heights[49] = { 155, 31, 74, 117, 24, 19, 198, 113, 181,
+			98, 23, 48, 20, 121, 26, 177, 60, 92, 5, 36, 32,
+			54, 85, 150, 20, 27, 112, 15, 73, 185, 8, 100, 15, 132,
+			95, 109, 47, 75, 74, 162, 43, 44, 99, 3, 6, 121, 32,
+			21, 129};
 
-	int heights[72] = { 305, 224, 174, 187, 231, 139, 298, 83, 218,
-		198, 63, 248, 320, 121, 326, 177, 160, 192, 55, 236, 332,
-		64, 108, 250, 320, 327, 20, 315, 273, 185, 18, 91, 315, 232,
-		295, 29, 347, 375, 174, 162, 43, 44, 299, 43, 6, 21, 320,
-		201, 29, 40, 58, 102, 383, 349, 145, 352, 290, 266, 56, 221,
-		197, 27, 245, 45, 305, 340, 235, 275, 400, 220, 123, 53 };
+	int size = 49;
+	SetTargetFPS(60);
 
 	while (!WindowShouldClose()) {
 		bool swapped;
-		for (int i = 0; i < 71; i++) {
+		for (int i = 0; i < size - 1; i++) {
 			swapped = false;
-			for (int j = 0; j < 71 - i; j++) {
+			for (int j = 0; j < size - 1 - i; j++) {
 				BeginDrawing();
-				DrawText("Bubble Sort", 10, 10, 20, DARKPURPLE);
+				DrawText("Bubble Sort", 5, 5, 22, DARKPURPLE);
 				ClearBackground(WHITE);
 				if (heights[j] > heights[j + 1]) {
 					std::swap(heights[j], heights[j + 1]);
 					swapped = true;
 				}
-				for (int k = 0; k < 72; k++) {
+				for (int k = 0; k < size; k++) {
 					if (k == j) {
-						DrawRectangle(7 + (k * 11), winHeight - heights[k], rectWidth, heights[k],RED);
+						DrawRectangle(2 + (k * 8), ((winHeight / 2) - 5) - heights[k], rectWidth, heights[k], RED);
 					}
 					else {
-						DrawRectangle(7 + (k * 11), winHeight - heights[k], rectWidth, heights[k], Color{ 53, 181, 172, 255 });
+						DrawRectangle(2 + (k * 8), ((winHeight / 2) - 5) - heights[k], rectWidth, heights[k], Color{ 53, 181, 172, 255 });
 					}
 				}
+				drawCross();
+				DrawFPS(winWidth - 30, 10);
 				EndDrawing();
 			}
 			if (!swapped) break;
@@ -47,14 +54,15 @@ int main() {
 
 	while (!WindowShouldClose()) {
 		BeginDrawing();
-		DrawText("Bubble Sort - Sorted!", 10, 10, 20, DARKPURPLE);
+		DrawText("Bubble Sort - Sorted!", 5, 5, 22, DARKPURPLE);
 		ClearBackground(WHITE);
-		for (int i = 0; i < 72; i++) {
-			DrawRectangle(7 + (i * 11), winHeight - heights[i], rectWidth, heights[i], Color{ 53, 181, 172, 255 });
+		for (int i = 0; i < size; i++) {
+			DrawRectangle(2 + (i * 8), ((winHeight / 2) - 5) - heights[i], rectWidth, heights[i], Color{ 53, 181, 172, 255 });
 		}
+		drawCross();
+		DrawFPS(winWidth - 30, 10);
 		EndDrawing();
 	}
+	
 	return 0;
 }
-
-// Implemented bubble sort algorithm and added visualisation for it.
