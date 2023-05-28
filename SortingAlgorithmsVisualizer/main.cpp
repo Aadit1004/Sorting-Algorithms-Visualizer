89f51b863ea5c35095d94081678637f476f3d5d0
Aadit Rao <aaditrao10@gmail.com>
2023-05-28 10:36:11 +0400

    Implemented bubble sort algorithm and added visualisation for it.

--- SortingAlgorithmsVisualizer/main.cpp
+++ SortingAlgorithmsVisualizer/main.cpp
@@ -1,5 +1,6 @@
 #include "raylib.h"
 #include <stdlib.h>
+#include <algorithm>
 #include <functional>
 
 int main() {
@@ -16,21 +17,44 @@ int main() {
 		295, 29, 347, 375, 174, 162, 43, 44, 299, 43, 6, 21, 320,
 		201, 29, 40, 58, 102, 383, 349, 145, 352, 290, 266, 56, 221,
 		197, 27, 245, 45, 305, 340, 235, 275, 400, 220, 123, 53 };
-	
 
 	while (!WindowShouldClose()) {
-		// Updated
+		bool swapped;
+		for (int i = 0; i < 71; i++) {
+			swapped = false;
+			for (int j = 0; j < 71 - i; j++) {
+				BeginDrawing();
+				DrawText("Bubble Sort", 10, 10, 20, DARKPURPLE);
+				ClearBackground(WHITE);
+				if (heights[j] > heights[j + 1]) {
+					std::swap(heights[j], heights[j + 1]);
+					swapped = true;
+				}
+				for (int k = 0; k < 72; k++) {
+					if (k == j) {
+						DrawRectangle(7 + (k * 11), winHeight - heights[k], rectWidth, heights[k],RED);
+					}
+					else {
+						DrawRectangle(7 + (k * 11), winHeight - heights[k], rectWidth, heights[k], Color{ 53, 181, 172, 255 });
+					}
+				}
+				EndDrawing();
+			}
+			if (!swapped) break;
+		}
+		break;
+	}
 
-		// Draw
+	while (!WindowShouldClose()) {
 		BeginDrawing();
+		DrawText("Bubble Sort - Sorted!", 10, 10, 20, DARKPURPLE);
 		ClearBackground(WHITE);
 		for (int i = 0; i < 72; i++) {
-			DrawRectangle(7 + (i * 11), winHeight - heights[i], rectWidth, heights[i], 
-				Color{53, 181, 172, 255});
+			DrawRectangle(7 + (i * 11), winHeight - heights[i], rectWidth, heights[i], Color{ 53, 181, 172, 255 });
 		}
-
 		EndDrawing();
-
 	}
 	return 0;
 }
+
+// Implemented bubble sort algorithm and added visualisation for it.
